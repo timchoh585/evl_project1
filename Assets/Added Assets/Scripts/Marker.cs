491deb50ec07ceae8d75a0ab3d221d284f2b2383
Edmond Zalenski <ezalen2@uic.edu>
2017-09-24 12:38:00 -0500

    removed extra scenes and moved main scene

---
+++ Assets/Added Assets/Scripts/Marker.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Marker : MonoBehaviour {
+	private Color color;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
