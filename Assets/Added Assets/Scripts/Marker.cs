ded23e416a668e3be022735fdd7bd88750cf711d
Edmond Zalenski <ezalen2@uic.edu>
2017-09-24 15:08:59 -0500

    merge scenes

--- Assets/Added Assets/Scripts/Marker.cs
+++ Assets/Added Assets/Scripts/Marker.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class Marker : MonoBehaviour {
 	private Color color;
+
 	// Use this for initialization
 	void Start () {
-		
+			
 	}
 	
 	// Update is called once per frame
