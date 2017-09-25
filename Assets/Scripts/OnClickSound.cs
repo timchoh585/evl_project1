4f4e30518bd41e0d41c0f9e506447d5d37984f01
ConditionalCoder <Georgepoulos5@gmail.com>
2017-09-25 01:56:47 -0500

    Added CollisionSound Trigger to Exho

---
+++ Assets/Scripts/OnClickSound.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnClickSound : MonoBehaviour {
+
+	public AudioClip OnTouchSound;
+
+	void OnTriggerEnter(Collider c){
+		AudioSource.PlayClipAtPoint (OnTouchSound, new Vector2 (0, 0));
+	}
+}
